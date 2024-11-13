cd55b1ae4af4b0ed3e28e783e550f00eb1621191
imjiwonim <95373233+imjiwonim@users.noreply.github.com>
2024-11-14 08:12:36 +0900

    Add files via upload

--- project/hobbyst/board/urls.py
+++ project/hobbyst/board/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from board.views import home, comment_add, comment_delete, post_write
-# ,post_write
+from board import views
+from board.views import home, comment_add, comment_delete, post_write, post_detail
 
 urlpatterns = [
     path('', home, name='home'),
-    path('comment_add/', comment_add),
+    path('comment_add/', views.comment_add, name='comment_add'),
     path('comment_delete/<int:comment_id>/', comment_delete),
-    path('post_write/', post_write),
+    path('post_write/', views.post_write, name='post_write'),
+    path("<int:post_id>/", post_detail, name="post_detail"),
 ]
--- project/hobbyst/board/views.py
+++ project/hobbyst/board/views.py
@@ -5,8 +5,6 @@ from django.views.decorators.http import require_POST
 from django.http import HttpResponseRedirect , HttpResponseForbidden
 
 
-# Create your views here.
-
 def home(request):
     # if not request.user.is_authenticated:
         # return redirect('/account/login')
@@ -19,6 +17,7 @@ def home(request):
     return render(request, 'board/home.html',context)
 
 def post_write(request):
+    form = PostForm()
     if request.method == "POST":
         # request.POST로 온 데이터 ("content")는 PostForm 으로 처리
         form = PostForm(request.POST)
@@ -27,7 +26,7 @@ def post_write(request):
             post = form.save(commit=False)
             post.user = request.user
             post.save()
-        # Post 생성 후 request.FILES.getlists("images")로ㅓ 전송된 이미지들을 순회하며 PostImage 객체를 생성한다.
+        # Post 생성 후 request.FILES.getlists("images")로 전송된 이미지들을 순회하며 PostImage 객체를 생성한다.
             for image_file in request.FILES.getlist("images"):
                 #request.FILES 로 가져온 파일은 Model의 ImageField 부분에 곧바로 할당한다.
                 PostImage.objects.create(
@@ -37,8 +36,8 @@ def post_write(request):
             url = f"/board/#post-{post.id}"
             return HttpResponseRedirect(url)
         
-        else :
-            form = PostForm()
+        # else :
+        #     form = PostForm()
     
     context = {'form': form}
     return render(request, "board/post_write.html", context)
@@ -49,30 +48,36 @@ def comment_add(request):
     # print(request.POST)
     form = CommentForm(data=request.POST)
     if form.is_valid():
-#         # 'post' 필드를 hidden input에서 가져옴
-#         post_id = request.POST.get('post')
-#         post = Post.objects.get(id=post_id)
+#        'post' 필드를 hidden input에서 가져옴
+        # post_id = request.POST.get('post')
+        # post = Post.objects.get(id=post_id)
 #         # Comment 객체를 생성하지만 아직 저장하지 않음 (commit=False)
         comment = form.save(commit=False)
-#         comment.post = post
+        # comment.post = post
         comment.user = request.user  # 현재 로그인한 사용자를 댓글 작성자로 설정
         comment.save()
         print(comment.id)
         print(comment.content)
         print(comment.user)
         # return redirect('board/')
-        return HttpResponseRedirect(f"/board/#post-{comment.post.id}")              # 교재에는 posts/feed/#post-{comment.post.id} 로 되있음 (헷갈림;posts/feeds > board? or board/home/ ?)
+        url = reverse("board:home") + f"#post-{comment.post.id}"
+        return HttpResponseRedirect(url_next)
+    # (f"/board/#post-{comment.post.id}")              # 교재에는 posts/feed/#post-{comment.post.id} 로 되있음 (헷갈림;posts/feeds > board? or board/home/ ?)
     
 @require_POST
 def comment_delete(request, comment_id):
     if request.method == 'POST':
         comment = Comment.objects.get(id=comment_id)
-        if comment.user ==request.user:
+        if comment.user == request.user:
             comment.delete()
             return HttpResponseRedirect(f"/board/#post-{comment.post.id}")
         else :
             return HttpResponseForbidden("이 댓글을 삭제할 권한이 없습니다.")
         
 
-def post_write(request):
-    return render(request, "board/post_write.html")
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    comment_form = CommentForm()
+    context = {"board":post,
+               "comment_form": comment_form,}
+    return render(request, "board/post_detail.html", context)
