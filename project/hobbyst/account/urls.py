9b29bab4b088db1ebe052e02631ca0d5b30836f3
imjiwonim <95373233+imjiwonim@users.noreply.github.com>
2024-11-13 15:22:25 +0900

    Add files via upload

--- project/hobbyst/account/urls.py
+++ project/hobbyst/account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from account.views import login_view, logout_view, signup
 
 urlpatterns = [
-    path('login/', login_view),
-    path('logout/', logout_view),
-    path('signup/', signup)
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('signup/', signup, name='signup'),
 ]
